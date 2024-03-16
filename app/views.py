547bd618849a265890e4c36cfd71acaf634468e2
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-16 14:06:48 +0530

    setup url , home

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import ImageUploader
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    all_obj = ImageUploader.objects.all()
+    return render(request, 'home.html', context={'all_obj':all_obj})

3a277c5290df1af79b47ac3fdf03352165462dfb
rahulsrajput <rahulsrajput29@gmail.com>
2024-03-16 14:50:10 +0530

    about & contact page done

--- app/views.py
+++ app/views.py
@@ -5,3 +5,9 @@ from .models import ImageUploader
 def home(request):
     all_obj = ImageUploader.objects.all()
     return render(request, 'home.html', context={'all_obj':all_obj})
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
--- imageuploader/urls.py
+++ imageuploader/urls.py
@@ -24,6 +24,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
